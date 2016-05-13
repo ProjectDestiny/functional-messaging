d38e33a4ea80ebde17fdb8329f454482f0c0d35a
Felipe Robledo <gamedev_fantasy@hotmail.com>
2016-05-12 22:41:24 -0700

    Trying out gtest.

--- gtest-sample.cpp
+++ gtest-sample.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "gtest/gtest.h"
 
-int main(void) {
-	
+TEST(Group1,Subtest1) {
+	EXPECT_EQ(1,0);
+}
+
+TEST(Group2,Subtest1) {
+	EXPECT_EQ(0,0);
+}
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	int result = RUN_ALL_TESTS();
+	return result;
 }
